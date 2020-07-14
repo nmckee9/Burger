77b45917e5407b09030e4b06bfc03ed248c721db
nat <nmckee9@gmail.com>
2020-07-14 09:58:44 -0700

    successfully adds input text into database on submit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('krabby patty');
+INSERT INTO burgers (burger_name) VALUES ('double krabby patty');
+INSERT INTO burgers (burger_name) VALUES ('triple krabby patty');
