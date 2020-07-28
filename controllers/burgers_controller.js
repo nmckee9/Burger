6b4ca981a27d890c2f59ac3e34da1b9e0038e6d3
nat <nmckee9@gmail.com>
2020-07-28 09:00:47 -0700

    finishes on click events

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,13 +10,11 @@ router.get("/", function(req, res) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  console.log(req)
   burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(
       result
   ) {
@@ -25,11 +23,13 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.put("/api/burgers/:id", function(req, res) {
+
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
   burger.update(
+    {
+      devoured: req.body.devoured
+    },
     condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,30 +1,28 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
+$(document).ready(function() {
+  $(".devour").on("click", function(event) {
+    event.preventDefault();
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var isDevoured = {
+      devoured: true
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: isDevoured
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $("#submitBurger").on("click", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-    console.log("clicked")
 
     var newBurger = {
       burger_name: $("#burgerType").val().trim()
@@ -36,22 +34,6 @@ $(function() {
       data: newBurger
     }).then(
       function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
         // Reload the page to get the updated list
         location.reload();
       }
